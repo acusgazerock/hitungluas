bd4e443160596a51df9e6254be85ccf4e610a661
acus <agus.jodi@gmail.com>
2016-10-14 16:05:18 +0700

    bug null, only dot

--- app/src/main/java/com/acusgazerock/hitungluas/MainActivity.java
+++ app/src/main/java/com/acusgazerock/hitungluas/MainActivity.java
@@ -1,10 +1,12 @@
 package com.acusgazerock.hitungluas;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtPanjang, edtLebar;
@@ -17,23 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
 
-        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
-        edtLebar = (EditText)findViewById(R.id.edt_lebar);
-        btnHitung = (Button)findViewById(R.id.btn_hitung);
-        txtLuas = (TextView)findViewById(R.id.txt_luas);
+        edtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
 
         btnHitung.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String panjang = edtPanjang.getText().toString().trim();
                 String lebar = edtLebar.getText().toString().trim();
+                if (panjang.matches("")) {
+                    Toast.makeText(MainActivity.this, "You did not enter a panjang", Toast.LENGTH_SHORT).show();
+                } else if (lebar.matches("")) {
+                    Toast.makeText(MainActivity.this, "You did not enter a lebar", Toast.LENGTH_SHORT).show();
+                } else  if (panjang.equals(".")) {
+                    Toast.makeText(MainActivity.this, "panjang must have value", Toast.LENGTH_SHORT).show();
+                } else if (lebar.equals(".")) {
+                    Toast.makeText(MainActivity.this, "lebar must have value", Toast.LENGTH_SHORT).show();
+                } else {
 
-                double p = Double.parseDouble(panjang);
-                double l = Double.parseDouble(lebar);
+                    double p = Double.parseDouble(panjang);
+                    double l = Double.parseDouble(lebar);
 
-                double luas = p * l;
+                    double luas = p * l;
 
-                txtLuas.setText("Luas : "+luas);
+                    txtLuas.setText("Luas : " + luas);
+                }
             }
         });
     }
